394f68aff51eba13a22d2e1a09d786e5dbf5ae29
Kirill Malskiy <sabbat@mail.ru>
2024-04-21 10:24:37 +0300

    в классе Main добавил метод getTaskByCategory(для выбора категории из которой нужно получить все задачи), в классе TaskManager добавил метод getAllTasks который в зависимоти от переданного аргумента печатает все задачки из данной категории

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         taskManager = new TaskManager();
         scanner = new Scanner(System.in);
 
-        while (true) {
+        while (true) { //запускаем цикл
             // печатаем менюшку
             printMenu();
             // в переменную command записываем данные считаные из консоли
@@ -23,17 +23,23 @@ public class Main {
                     createNewTask();
                     break;
                 case "2":
+                    getTaskByCategory();
+                    break;
+                case "10":
                     return;
                 default:
                     System.out.println("Такой команды еще нет :)");
+
             }
         }
     }
 
     private static void printMenu() { // метод печати менюшки
         System.out.println("Выберите команду");
-        System.out.println("1: Создать задачу");
-        System.out.println("2 - Выход");
+        System.out.println("1 - Создать задачу");
+        System.out.println("2 - Посмотреть все задачи");
+        System.out.println("10 - Выход");
+
     }
 
     private static void createNewTask() { // метод создания задачки
@@ -44,5 +50,23 @@ public class Main {
         Task task = new Task(name, description); // создаем обьект класса Task с аргументами имя и описание
 
         taskManager.addNewTask(task); // у менеджера задач вызываем метод добавление задачки
+    }
+
+    private static void getTaskByCategory() { // метод получения задачек из категории
+        System.out.println("Введите категорию задач которые вы хотите распечатать: ");
+        System.out.println("1 - простые");
+        System.out.println("2 - эпики");
+        String category = scanner.nextLine().trim(); // считываем имя
+
+        switch (category) { // проверяем категорию
+            case "1":
+                taskManager.getAllTasks("простые");  
+                break;
+            case "2":
+                taskManager.getAllTasks("эпики");
+                break;
+            default:
+                System.out.println("Такой категории нет :)");
+        }
     }
 }
--- src/kanban/service/TaskManager.java
+++ src/kanban/service/TaskManager.java
@@ -21,6 +21,26 @@ public class TaskManager {
         tasksList.put(id, task);
     }
 
+    public void getAllTasks(String category) { // получаем все задачки из категории
+        if (category.equals("простые")) { // проверяем категорию
+            if (tasksList.isEmpty()) { // проверяем что хешмапа не пустая
+                System.out.println("Простых задачек пока нет");
+                return; // вылетаем
+            }
+
+            for (Task task : tasksList.values()) { // провегаемся по значениям хешмапы и печатаем
+                System.out.println("Задача -" + task.getName()); // имя
+                System.out.println("Описание - " + task.getDescription()); // описание
+                System.out.println("Статус - " + task.getStatus()); // статус
+                System.out.println("");
+            }
+        }
+    }
+
+//    public void removeAllTasks() {
+//
+//    }
+
     @Override
     public String toString() {
         String taskName = "";
