55011c5f3ea27b838ecf992334182b4754c3b49d
Kirill Malskiy <sabbat@mail.ru>
2024-04-30 16:04:19 +0300

    правки второго ревью(в классе Main добавил завершение проверки)

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,9 @@ import kanban.model.SubTask;
 import kanban.model.Task;
 import kanban.service.TaskManager;
 
-import java.util.Scanner;
-
 public class Main {
 
     static TaskManager taskManager;
-    static Scanner scanner;
 
     public static void main(String[] args) {
         taskManager = new TaskManager();
@@ -23,13 +20,13 @@ public class Main {
             "Поехать в отпуск с семьей");
         taskManager.addNewEpic(firstEpic);
 
-        SubTask firstSubtask = new SubTask(
+        SubTask firstSubTask = new SubTask(
         "Взять семью", "Жена, дочка", Status.NEW, 3);
-        taskManager.addNewSubTask(firstSubtask);
+        taskManager.addNewSubTask(firstSubTask);
 
-        SubTask secondSubtask = new SubTask(
+        SubTask secondSubTask = new SubTask(
             "Взять собак", "Пэди, Потап, Дик", Status.DONE, 3);
-        taskManager.addNewSubTask(secondSubtask);
+        taskManager.addNewSubTask(secondSubTask);
 
         System.out.println("1 - Получение списков всех задач");
         System.out.println(taskManager.getAllTasks());
@@ -50,6 +47,49 @@ public class Main {
         System.out.println(taskManager.getAllEpics());
         System.out.println(taskManager.getAllSubTask());
 
+        System.out.println("4 Создание задачек");
+        Task newFirstTask = taskManager.createTask(firstTask);
+        taskManager.addNewTask(newFirstTask);
+        Task newSecondTask = taskManager.createTask(secondTask);
+        taskManager.addNewTask(newSecondTask);
 
+        Epic newEpic = taskManager.createEpic(firstEpic);
+        taskManager.addNewEpic(newEpic);
+
+        SubTask firdSubTask = new SubTask(
+            "Взять семью", "Жена, дочка", Status.NEW, 8);
+        SubTask fourthSubTask = new SubTask(
+            "Взять собак", "Пэди, Потап, Дик", Status.DONE, 8);
+
+        SubTask newFirstSubTask = taskManager.createSubTask(firdSubTask);
+        taskManager.addNewSubTask(newFirstSubTask);
+        SubTask newSecondSubTask = taskManager.createSubTask(fourthSubTask);
+        taskManager.addNewSubTask(newSecondSubTask);
+
+        System.out.println(newFirstTask);
+        System.out.println(newSecondTask);
+        System.out.println(newEpic);
+        System.out.println(newFirstSubTask);
+        System.out.println(newSecondSubTask);
+
+        System.out.println("5 Обновление задачек");
+
+        taskManager.updateTask(new Task("Отвести дочку в школу", "Не забыть портфель и сменку и форму", 6, Status.IN_PROGRESS));
+        taskManager.updateEpic(new Epic("Поехать в отпуск на море", "Поехать в отпуск с семьей", 8));
+
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllEpics());
+        System.out.println(taskManager.getAllSubTask());
+
+        System.out.println("6 Получение списка всех подзадач определённого эпика");
+        System.out.println(taskManager.getAllSubtasksByEpic(8));
+
+        System.out.println("7 Удаление по id");
+        taskManager.removeTaskById(6);
+//        taskManager.removeEpicById(8);
+        taskManager.removeSubTaskById(9);
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllEpics());
+        System.out.println(taskManager.getAllSubTask());
     }
 }
