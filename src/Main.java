70a6b411780cb16de121736247b58843c9e78ef4
Kirill Malskiy <sabbat@mail.ru>
2024-04-30 15:12:53 +0300

    правки второго ревью(в классе Main добавил проверку)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import kanban.model.Epic;
 import kanban.model.Status;
+import kanban.model.SubTask;
 import kanban.model.Task;
 import kanban.service.TaskManager;
 
@@ -12,8 +14,42 @@ public class Main {
     public static void main(String[] args) {
         taskManager = new TaskManager();
 
-        Task task = new Task("name", "description", Status.NEW);
+        Task firstTask = new Task("Отвести дочку в школу", "Не забыть портфель и сменку", Status.NEW);
+        taskManager.addNewTask(firstTask);
+        Task secondTask = new Task("Сходить на бокс", "Не получить по голове", Status.NEW);
+        taskManager.addNewTask(secondTask);
 
-        taskManager.addNewTask(task);
+        Epic firstEpic = new Epic("Поехать в отпуск",
+            "Поехать в отпуск с семьей");
+        taskManager.addNewEpic(firstEpic);
+
+        SubTask firstSubtask = new SubTask(
+        "Взять семью", "Жена, дочка", Status.NEW, 3);
+        taskManager.addNewSubTask(firstSubtask);
+
+        SubTask secondSubtask = new SubTask(
+            "Взять собак", "Пэди, Потап, Дик", Status.DONE, 3);
+        taskManager.addNewSubTask(secondSubtask);
+
+        System.out.println("1 - Получение списков всех задач");
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllEpics());
+        System.out.println(taskManager.getAllSubTask());
+
+        System.out.println("2 - Получение по идентификатору");
+        System.out.println(taskManager.getTaskById(1));
+        System.out.println(taskManager.getEpicById(3));
+        System.out.println(taskManager.getSubTaskById(4));
+
+        taskManager.removeAllTasks();
+        taskManager.removeAllEpics();
+        taskManager.removeAllSubTasks();
+
+        System.out.println("3 Удаление всех задач");
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllEpics());
+        System.out.println(taskManager.getAllSubTask());
+
+
     }
 }
