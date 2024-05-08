60c2b0148e260fc34b0537c9b01f71d152eaa543
Kirill Malskiy <sabbat@mail.ru>
2024-05-08 12:27:25 +0300

    правки первого ревью(внес исправления в класс InMemoryHistoryManager)

--- src/kanban/service/InMemoryHistoryManager.java
+++ src/kanban/service/InMemoryHistoryManager.java
@@ -6,17 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager{
-    private ArrayList<Task> historyTasksList = new ArrayList<>();
+    private List<Task> historyTasksList = new ArrayList<>();
     private int MAX_SIZE_HISTORY = 10;
     @Override
     public void addTask(Task task) {
         if (task != null) { // проверка на null
-            if (historyTasksList.size() < MAX_SIZE_HISTORY) { // если список не превышает максимального размера
-                historyTasksList.add(task); // добавляем туда задачку
-            } else { // ежели превышает
+            if (historyTasksList.size() >= MAX_SIZE_HISTORY) { // если список равен или больше максимального размера
                 historyTasksList.remove(0); // удаляем первую в списке
-                historyTasksList.add(task); // добавляем в конец новую
-            }
+            } 
+            historyTasksList.add(task); // добавляем в список задачку
         }
     }
 
