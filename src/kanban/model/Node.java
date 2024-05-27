192dd869717904d4d471dff6594a906c9452f1b4
Kirill Malskiy <sabbat@mail.ru>
2024-05-27 13:47:44 +0300

    в пакет model добавил класс Node

---
+++ src/kanban/model/Node.java
@@ -0,0 +1,14 @@
+package kanban.model;
+
+public class Node<E> {
+    E data;
+    Node<E> prev;
+    Node<E> next;
+
+
+    public Node(Node<E> prev, E data, Node<E> next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+}
