06993a43cbb08ae57d54f580d07af1c57c4a409e
Kirill Malskiy <sabbat@mail.ru>
2024-05-28 11:26:24 +0300

    правки после тестирования на гитхаб2

--- src/kanban/service/HistoryManager.java
+++ src/kanban/service/HistoryManager.java
@@ -1,13 +1,12 @@
 package kanban.service;
 
-import kanban.model.Node;
 import kanban.model.Task;
-
 import java.util.List;
-import java.util.Map;
 
 public interface HistoryManager {
     void addTask(Task task);
+
     void remove(int id);
+
     List<Task> getHistory();
 }
