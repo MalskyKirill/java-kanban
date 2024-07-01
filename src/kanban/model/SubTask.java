7cc150040c74a5bfa6c4d798f1cb33ae6e885d1e
Kirill Malskiy <sabbat@mail.ru>
2024-07-01 14:38:19 +0300

    правки первого ревью(в классе SubTask переопределил два родительских конструктора без времени)

--- src/kanban/model/SubTask.java
+++ src/kanban/model/SubTask.java
@@ -8,17 +8,29 @@ public class SubTask extends Task {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm");
     private int epicId; // айдишник епика которому принадлежин подзадача
 
-    //переопредилил консруктор обработки подзадачи менеджером
+    //переопредилил консруктор обработки подзадачи менеджером со временем
     public SubTask(String name, String description, int id, Status status, int epicId, LocalDateTime startTime, Duration duration) {
         super(name, description, id, status, startTime, duration);
 
         this.epicId = epicId;
     }
 
-    //переопредилил консруктор создания подзадачки
+    //переопредилил консруктор создания подзадачки со временем
     public SubTask(String name, String description, Status status, LocalDateTime startTime, Duration duration, int epicId) {
         super(name, description, status, startTime, duration);
 
+        this.epicId = epicId;
+    }
+
+    //переопредилил консруктор обработки подзадачи менеджером без времени
+    public SubTask(String name, String description, int id, Status status, int epicId) {
+        super(name, description, id, status);
+        this.epicId = epicId;
+    }
+
+    //переопредилил консруктор создания подзадачки без времени
+    public SubTask(String name, String description, Status status, int epicId) {
+        super(name, description, status);
         this.epicId = epicId;
     }
 
